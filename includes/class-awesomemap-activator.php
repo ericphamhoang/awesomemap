41e0750876fe7cc9d2d5c60bec3d3a846bb1647c
eric <eric>
2016-02-02 10:37:16 +1100

    Init gg map

---
+++ includes/class-awesomemap-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/ericphamhoang
+ * @since      1.0.0
+ *
+ * @package    Awesomemap
+ * @subpackage Awesomemap/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Awesomemap
+ * @subpackage Awesomemap/includes
+ * @author     Eric Pham Hoang <ericphamhoang@gmail.com>
+ */
+class Awesomemap_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-awesomemap-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/ericphamhoang
+ * @since      1.0.0
+ *
+ * @package    Awesomemap
+ * @subpackage Awesomemap/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Awesomemap
+ * @subpackage Awesomemap/includes
+ * @author     Eric Pham Hoang <ericphamhoang@gmail.com>
+ */
+class Awesomemap_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
